7d34a9bb96ee97e539cd79fcec2b8f04d96fe5e2
igotis <igotis@ukr.net>
2021-03-12 13:23:49 +0200

    feat: add readable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
-  
+  let   result = '',
+        one_units   = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight","nine"],
+        two_units   = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen","sixteen", "seventeen", "eighteen", "nineteen"],
+        tens        = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];    
+
+
+    if (number >= 100 ) {
+        result = one_units[Math.floor(number/100)] + ' ' + 'hundred'+' ';
+        if (number%100 !==0) {
+            number = number%100;
+        }
+    }
+
+    if (number >= 0 && number <10) {
+        result += one_units[number];
+    }
+
+    if(number >= 10 && number < 20) {
+        result += two_units[number%10];
+    }
+
+    if(number >=20 && number <100) {
+        let twentyToHundred = tens[(Math.floor(number/10))];
+        if (number%10 == 0){
+            result += tens[(Math.floor(number/10))];
+        } else {
+            result += twentyToHundred +' '+ one_units[(number%10)];
+        }        
+    }   
+
+return result.trim();
 }
